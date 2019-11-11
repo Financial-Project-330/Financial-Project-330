04424ec30718caa40e04d120bc4f39dbd29e453d
Lucah Ueno <lucahueno2022@u.northwestern.edu>
2019-11-11 13:38:07 -0600

    fixed all bugs

--- finance.js
+++ finance.js
@@ -1,20 +1,23 @@
 var total = 0;
 var items = [];
+var i = 0;
 
 function subtract() {
-  var expenditure = document.getElementById('expenditure');
-  document.getElementById('expenditure') = '';
-  total = total - Number(expenditure);
-  document.getElementById('total').value = total;
-  items.push(-Number(expenditure));
+  var expenditure = Number(document.getElementById('expenditure').value);
+  document.getElementById('expenditure').value = '';
+  total = total - expenditure;
+  document.getElementById('total').innerHTML = '$' + total;
+  items.push(-expenditure);
   document.getElementById('items').innerHTML += '<li>' + '$' + items[i] + '</li>';
+  i++;
 }
 
 function add() {
-  var income = document.getElementById('income');
-  document.getElementById('income') = '';
-  total = total + Number(income);
-  document.getElementById('total').value = total;
-  items.push(Number(income));
+  var income = Number(document.getElementById('income').value);
+  document.getElementById('income').value = '';
+  total = total + income;
+  document.getElementById('total').innerHTML = '$' + total;
+  items.push(income);
   document.getElementById('items').innerHTML += '<li>' + '$' + items[i] + '</li>';
+  i++;
 }
