904f0785e98a0c0a919872902fb1f77d96923785
Lucah Ueno <lucahueno2022@u.northwestern.edu>
2019-11-11 13:11:57 -0600

    fixed bug

--- finance.js
+++ finance.js
@@ -7,9 +7,7 @@ function subtract() {
   total = total - Number(expenditure);
   document.getElementById('total').value = total;
   items.push(-Number(expenditure));
-  for (i=0; i<items.length; i++) {
-    document.getElementById('items').innerHTML += '<li>' + '$' + items[i] + '</li>';
-  }
+  document.getElementById('items').innerHTML += '<li>' + '$' + items[i] + '</li>';
 }
 
 function add() {
@@ -18,7 +16,5 @@ function add() {
   total = total + Number(income);
   document.getElementById('total').value = total;
   items.push(Number(income));
-  for (i=0; i<items.length; i++) {
-    document.getElementById('items').innerHTML += '<li>' + '$' + items[i] + '</li>';
-  }
+  document.getElementById('items').innerHTML += '<li>' + '$' + items[i] + '</li>';
 }
