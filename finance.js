a2de703604bc781f04027bffbf0148ba9d0e9c35
Lucah Ueno <lucahueno2022@u.northwestern.edu>
2019-11-11 12:55:09 -0600

    Update to numerical values

--- finance.js
+++ finance.js
@@ -2,27 +2,23 @@ var total = 0;
 var items = [];
 
 function subtract() {
-  var expenditure = -(document.getElementById('expenditure'));
-  if (!isNaN(expenditure)) {
-    document.getElementById('expenditure') = '';
-    total = total + expenditure;
-    document.getElementById('total').value = total;
-    items.push(expenditure);
-    for (i=0; i<items.length; i++) {
-      document.getElementById('items').innerHTML += '<li>' + items[i] + '</li>';
-    }
+  var expenditure = -Number((document.getElementById('expenditure')));
+  document.getElementById('expenditure') = '';
+  total = total + expenditure;
+  document.getElementById('total').value = total;
+  items.push(expenditure);
+  for (i=0; i<items.length; i++) {
+    document.getElementById('items').innerHTML += '<li>' + items[i] + '</li>';
   }
 }
 
 function add() {
-  var income = document.getElementById('income');
-  if (!isNaN(income)) {
-    document.getElementById('income') = '';
-    total = total + income;
-    document.getElementById('total').value = total;
-    items.push(income);
-    for (i=0; i<items.length; i++) {
-      document.getElementById('items').innerHTML += '<li>' + items[i] + '</li>';
-    }
+  var income = Number(document.getElementById('income'));
+  document.getElementById('income') = '';
+  total = total + income;
+  document.getElementById('total').value = total;
+  items.push(income);
+  for (i=0; i<items.length; i++) {
+    document.getElementById('items').innerHTML += '<li>' + items[i] + '</li>';
   }
 }
