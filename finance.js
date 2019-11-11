ff23217159ccb6665a63c3e49cc3c9808dc79c80
Lucah Ueno <lucahueno2022@u.northwestern.edu>
2019-11-11 14:21:08 -0600

    updated everything
    everything works fine. I tested. Please merge.
    
    The only thing we need to do is to add more css code to make it look nice.

--- finance.js
+++ finance.js
@@ -6,9 +6,14 @@ function subtract() {
   var expenditure = Number(document.getElementById('expenditure').value);
   document.getElementById('expenditure').value = '';
   total = total - expenditure;
-  document.getElementById('total').innerHTML = '$' + total;
+  if (total >= 0) {
+    document.getElementById('total').innerHTML = 'Total: $' + total;
+  }
+  else {
+    document.getElementById('total').innerHTML = 'Total: -$' + Math.abs(total);
+  }
   items.push(-expenditure);
-  document.getElementById('items').innerHTML += '<li>' + '$' + items[i] + '</li>';
+  document.getElementById('items').innerHTML += '<li>' + '-$' + Math.abs(items[i]) + '</li>';
   i++;
 }
 
@@ -16,7 +21,12 @@ function add() {
   var income = Number(document.getElementById('income').value);
   document.getElementById('income').value = '';
   total = total + income;
-  document.getElementById('total').innerHTML = '$' + total;
+  if (total >= 0) {
+    document.getElementById('total').innerHTML = 'Total: $' + total;
+  }
+  else {
+    document.getElementById('total').innerHTML = 'Total: -$' + Math.abs(total);
+  }
   items.push(income);
   document.getElementById('items').innerHTML += '<li>' + '$' + items[i] + '</li>';
   i++;
