77de66643aefaf4f500fd19325cd132a964a5a71
Lucah Ueno <lucahueno2022@u.northwestern.edu>
2019-11-11 11:43:43 -0600

    made number dissappear after hitting + or -

--- finance.js
+++ finance.js
@@ -3,20 +3,26 @@ var items = [];
 
 function subtract() {
   var expenditure = document.getElementById('expenditure');
-  total = total - expenditure;
-  document.getElementById('total').value = total;
-  items.push(-expenditure);
-  for (i=0; i<items.length; i++) {
-    document.getElementById('items').innerHTML += '<li>' + items[i] + '</li>';
+  if (!isNaN(expenditure)) {
+    document.getElementById('expenditure') = '';
+    total = total - expenditure;
+    document.getElementById('total').value = total;
+    items.push(-expenditure);
+    for (i=0; i<items.length; i++) {
+      document.getElementById('items').innerHTML += '<li>' + items[i] + '</li>';
+    }
   }
 }
 
 function add() {
   var income = document.getElementById('income');
-  total = total + income;
-  document.getElementById('total').value = total;
-  items.push(+income);
-  for (i=0; i<items.length; i++) {
-    document.getElementById('items').innerHTML += '<li>' + items[i] + '</li>';
+  if (!isNaN(income)) {
+    document.getElementById('income') = '';
+    total = total + income;
+    document.getElementById('total').value = total;
+    items.push(+income);
+    for (i=0; i<items.length; i++) {
+      document.getElementById('items').innerHTML += '<li>' + items[i] + '</li>';
+    }
   }
 }
