0f207875a6d3a78e2de8deda99868d0a1aa1e028
Lucah Ueno <lucahueno2022@u.northwestern.edu>
2019-11-11 11:35:21 -0600

    javascirpt added

--- finance.js
+++ finance.js
@@ -1,1 +1,22 @@
+var total = 0;
+var items = [];
 
+function subtract() {
+  var expenditure = document.getElementById('expenditure');
+  total = total - expenditure;
+  document.getElementById('total').value = total;
+  items.push(-expenditure);
+  for (i=0; i<items.length; i++) {
+    document.getElementById('items').innerHTML += '<li>' + items[i] + '</li>';
+  }
+}
+
+function add() {
+  var income = document.getElementById('income');
+  total = total + income;
+  document.getElementById('total').value = total;
+  items.push(+income);
+  for (i=0; i<items.length; i++) {
+    document.getElementById('items').innerHTML += '<li>' + items[i] + '</li>';
+  }
+}
