bf6951861e22d74f28cb16b8e2c3efe7e7ce00db
tschachner28 <53120075+tschachner28@users.noreply.github.com>
2019-11-12 11:19:02 -0600

    added reward for reaching goal balance

--- finance.js
+++ finance.js
@@ -1,16 +1,17 @@
 var total = 0;
 var items = [];
 var i = 0;
+var smile_reward = ''; //new
 
 function subtract() {
   var expenditure = Number(document.getElementById('expenditure').value);
   document.getElementById('expenditure').value = '';
   total = total - expenditure;
   if (total >= 0) {
-    document.getElementById('total').innerHTML = 'Total: $' + total;
+    document.getElementById('total').innerHTML = 'Balance: $' + total;
   }
   else {
-    document.getElementById('total').innerHTML = 'Total: -$' + Math.abs(total);
+    document.getElementById('total').innerHTML = 'Balance: -$' + Math.abs(total);
   }
   items.push(-expenditure);
   document.getElementById('items').innerHTML += '<li>' + '-$' + Math.abs(items[i]) + '</li>';
@@ -22,12 +23,18 @@ function add() {
   document.getElementById('income').value = '';
   total = total + income;
   if (total >= 0) {
-    document.getElementById('total').innerHTML = 'Total: $' + total;
+    document.getElementById('total').innerHTML = 'Balance: $' + total;
   }
   else {
-    document.getElementById('total').innerHTML = 'Total: -$' + Math.abs(total);
+    document.getElementById('total').innerHTML = 'Balance: -$' + Math.abs(total);
   }
   items.push(income);
   document.getElementById('items').innerHTML += '<li>' + '$' + items[i] + '</li>';
   i++;
+}
+
+function compare_total_to_goal() { //new
+  var goal = Number(document.getElementById('goalinput').value);
+  document.getElementById('goal').innerHTML = 'Goal Balance: $' + goal;
+  if (goal != 0 && total >= goal) document.getElementById('goal').innerHTML += '\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + 'Goal reached!' + '&#128513;';
 }
